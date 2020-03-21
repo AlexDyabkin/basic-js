0e9cccdf38e763da0a7ba8005ec1518e57d6fc40
AlexDyabkin <alexendredyabkin@gmail.com>
2020-03-21 12:49:10 +0300

    Update DreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,15 @@
 module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+  if(!Array.isArray(members)) {
+    return false;
+  }
+
+  var dreamTeam = [];
+
+  for(var i = 0; i < members.length; i++){
+    if(typeof members[i] == 'string'){
+      //var a = members[i].trim()[0].toUpperCase();
+      dreamTeam.push(members[i].trim()[0].toUpperCase());
+    }
+  }
+  return dreamTeam.sort().join("");
 };

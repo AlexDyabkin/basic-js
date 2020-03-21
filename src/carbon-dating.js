0fcf5b9b5157fc7439ff43b7c99228b05e0924b3
AlexDyabkin <alexendredyabkin@gmail.com>
2020-03-21 12:46:40 +0300

    add function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,6 +2,10 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+  if(typeof sampleActivity !== 'string' || Number(sampleActivity) > 15 || Number(sampleActivity) < 1 || isNaN(parseFloat(sampleActivity))){
+    return false;
+}
+
+var a = parseFloat(sampleActivity);
+return Math.ceil(Math.log(MODERN_ACTIVITY / a) / (0.693 / HALF_LIFE_PERIOD));
 };

54225f1d57135263c23ea2886bc361a9dcc2b96d
AlexDyabkin <alexendredyabkin@gmail.com>
2020-03-22 14:57:55 +0300

    Update carbonDating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,11 +1,13 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15; 
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  if(typeof sampleActivity !== 'string' || Number(sampleActivity) > 15 || Number(sampleActivity) < 1 || isNaN(parseFloat(sampleActivity))){
-    return false;
-}
+module.exports = function dateSample(sampleActivity) {
 
-var a = parseFloat(sampleActivity);
-return Math.ceil(Math.log(MODERN_ACTIVITY / a) / (0.693 / HALF_LIFE_PERIOD));
+    if(typeof sampleActivity !== 'string' || Number(sampleActivity) > 15 || Number(sampleActivity) < 1 || isNaN(parseFloat(sampleActivity))){
+        return false;
+    }
+
+    var a = parseFloat(sampleActivity);
+    return Math.ceil(Math.log(MODERN_ACTIVITY / a) / (0.693 / HALF_LIFE_PERIOD));
+
 };
